348d1c6b78e6325d4afdf0f626f0455b38515c5a
Pistos <git dawt pistos aet purepistos dawt net>
2016-06-23 16:39:36 -0500

    Add ability to run specific test files

--- backend/bin/run-tests.sh
+++ backend/bin/run-tests.sh
@@ -1,1 +1,9 @@
-PROJECT_NAME_ENV=test bundle exec rspec "$@"
+#!/bin/bash
+
+if (($#)); then
+  RSPEC_ARGS=("$@")
+else
+  RSPEC_ARGS=(--pattern 'spec/**/*-spec.rb')
+fi
+
+PROJECT_NAME_ENV=test bundle exec rspec "${RSPEC_ARGS[@]}"
