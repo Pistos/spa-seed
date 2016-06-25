9fbdc6d094a94d8d955955eacf46ce385d970ae2
Pistos <git dawt pistos aet purepistos dawt net>
2016-06-25 10:32:03 -0500

    Add specs for thing creation

---
+++ backend/spec/e2e/things/create-spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe 'Things: Create', :type => :feature, js: true do
+  context 'given a signed-in user' do
+    include_context 'given a signed-in user'
+
+    let(:name) { 'thing name' }
+    let(:description) { 'description of the thing' }
+
+    scenario 'creating things' do
+      visit 'http://localhost:3010/#!/home'
+
+      expect(page).not_to have_content(name)
+      expect(page).not_to have_content(description)
+
+      fill_in 'name', with: name
+      fill_in 'description', with: description
+
+      click_button 'Create'
+
+      expect(page).to have_content(name)
+      expect(page).to have_content(description)
+    end
+  end
+end
