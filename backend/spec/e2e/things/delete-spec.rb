57694bd1f83226e23467b970dc5e9150518923f4
Pistos <git dawt pistos aet purepistos dawt net>
2016-06-26 00:01:06 -0500

    Use more lets in thing deletion spec

--- backend/spec/e2e/things/delete-spec.rb
+++ backend/spec/e2e/things/delete-spec.rb
@@ -5,24 +5,26 @@ describe 'Things: Delete', :type => :feature, js: true do
     include_context 'given a signed-in user'
 
     context 'given some things' do
-      let!(:thing1) { FactoryGirl.create(:thing, name: 'particular name 1') }
-      let!(:thing2) { FactoryGirl.create(:thing, name: 'particular name 2') }
+      let(:name1) { 'particular name 1' }
+      let(:name2) { 'particular name 2' }
+      let!(:thing1) { FactoryGirl.create(:thing, name: name1) }
+      let!(:thing2) { FactoryGirl.create(:thing, name: name2) }
 
       scenario 'deleting things' do
         visit 'http://localhost:3010/#!/home'
 
-        expect(page).to have_content(thing1.name)
-        expect(page).to have_content(thing2.name)
+        expect(page).to have_content(name1)
+        expect(page).to have_content(name2)
 
         click_button 'delete', :match => :first
 
-        expect(page).not_to have_content(thing1.name)
-        expect(page).to have_content(thing2.name)
+        expect(page).not_to have_content(name1)
+        expect(page).to have_content(name2)
 
         click_button 'delete', :match => :first
 
-        expect(page).not_to have_content(thing1.name)
-        expect(page).not_to have_content(thing2.name)
+        expect(page).not_to have_content(name1)
+        expect(page).not_to have_content(name2)
       end
     end
   end
