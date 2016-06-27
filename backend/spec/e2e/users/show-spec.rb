bb4816f9d77ca4e1e2851ddb97179ef6c5455be5
Pistos <git dawt pistos aet purepistos dawt net>
2016-06-27 16:39:27 -0500

    Remove unnecessary sleeps and reload

--- backend/spec/e2e/users/show-spec.rb
+++ backend/spec/e2e/users/show-spec.rb
@@ -6,10 +6,6 @@ describe 'Users: Show', :type => :feature, js: true do
 
     scenario "viewing one's own page" do
       visit "http://localhost:3010/#!/users/#{user.id}"
-      sleep 3
-      # TODO: This is just temporary until user record deltas are propagated with the DatabaseEvent system
-      page.execute_script 'window.location.reload()'
-      sleep 3
 
       expect(page).to have_content(user.username)
     end
